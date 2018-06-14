9a82ff44e8ca2e15273ef26d8237ebd0517f0b06
tharindutcj <tharindutcj@gmail.com>
2018-06-14 22:28:42 +0530

    Added search name by name

--- routes/employee.js
+++ routes/employee.js
@@ -24,7 +24,7 @@ router.get('/', (req, res) => {
 });
 
 
-/*GET single employee details using _id */
+/*GET single employee details using eid */
 router.get('/:id', (req, res) => {
   models.Employee.find({ eid: req.params.id }, { _id: 0, __v: 0 }, (err, data) => {
     if (err) {
@@ -32,6 +32,21 @@ router.get('/:id', (req, res) => {
     }
     else if (data.length === 0) {
       res.status(404).send({ success:false, data: 'Invalid id provided.' });
+    }
+    else {
+      res.status(200).send({ success: true, data: data });
+    }
+  });
+});
+
+/*GET single employee details using eid */
+router.get('/name/:name', (req, res) => {
+  models.Employee.find({ name: {'$regex' : '.*'+req.params.name+'.*'}}, { _id: 0, __v: 0 }, (err, data) => {
+    if (err) {
+      res.status(500).send({ success: false, data: 'Internal error : ' + err });
+    }
+    else if (data.length === 0) {
+      res.status(404).send({ success:false, data: 'No matches found for '+req.params.name });
     }
     else {
       res.status(200).send({ success: true, data: data });
